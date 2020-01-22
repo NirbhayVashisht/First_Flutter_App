c2d08cc411d7575fb33d0cfa140dae5ada8b578e
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-01-22 19:57:23 +0530

    created a basic app that outputs Hello World

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  //runApp takes the object of our widget and calls the build method for us 
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //StatelessWidget class allows us to make our own widget 
+  //by extending it it turns our class into a widget which can be used by flutter
+
+  //We have to override build function after extending Stateless widget class
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    
+    //we are returning MaterialApp widget that does some base setup to turn our combination of widgets into a real app that can be rendered
+    //MaterialApp takes named arguments
+    //Basically we are calling the MaterialApp constructor here
+    return MaterialApp(home: Text('Hello World'));  
+
+  }
+
+}
